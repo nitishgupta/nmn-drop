846c4882a81828264dbbe5ccf4f7c2717d1a7a1a
Nitish Gupta <gnnitish@gmail.com>
2020-10-06 16:34:35 -0400

    cleaned up some data

--- datasets/drop/analysis/answer_type.py
+++ datasets/drop/analysis/answer_type.py
@@ -5,6 +5,8 @@ from collections import defaultdict
 from datasets.drop import constants
 
 
+interesting_tokens = [" the most ", " atleast "]
+
 def answerTypeAnalysis(input_json: str) -> None:
     """ Perform some analysis on answer types. """
 
@@ -16,45 +18,21 @@ def answerTypeAnalysis(input_json: str) -> None:
 
     print("Number of docs: {}".format(len(dataset)))
 
-    anstypes_count = defaultdict(int)
-    num_pspan_ans = 0
-    num_qspan_ans = 0
-    num_bothspan_ans = 0
-    num_nospan_ans = 0
-    num_qa = 0
-    spanans_numspandist = defaultdict(int)
+    numq = 0
+    num_relevant_q = 0
 
-    for passage_id, passage_info in dataset.items():
-        qa_pairs = passage_info[constants.qa_pairs]
-        for qa in qa_pairs:
-            num_qa += 1
-            ans_type = qa[constants.answer_type]
-            anstypes_count[ans_type] += 1
 
-            answer_dict = qa[constants.answer]
+    for pid, pinfo in dataset.items():
+        for qa in pinfo[constants.qa_pairs]:
+            numq += 1
+            question = qa[constants.question]
+            if any([x in question for x in interesting_tokens]):
+                print(question)
+                num_relevant_q += 1
 
-            if ans_type == constants.NUM_TYPE:
-                print(qa)
 
-            if ans_type == constants.SPAN_TYPE:
-                answer_span_texts = answer_dict["spans"]
-                spanans_numspandist[len(answer_span_texts)] += 1
-                q_ans_spans = qa[constants.answer_question_spans]
-                p_ans_spans = qa[constants.answer_passage_spans]
-                if q_ans_spans and p_ans_spans:
-                    num_bothspan_ans += 1
-                elif q_ans_spans:
-                    num_qspan_ans += 1
-                elif p_ans_spans:
-                    num_pspan_ans += 1
-                else:
-                    num_nospan_ans += 1
-
-    print(f"Num of QA:{num_qa}")
-    print(f"Answer Types: {anstypes_count}")
-    print("Among span answers:")
-    print(f"PSpans: {num_pspan_ans}. QSpans:{num_qspan_ans}, BothSpans:{num_bothspan_ans}, NoSpans:{num_nospan_ans}")
-    print(f"SpanType ans - number of correct spans: {spanans_numspandist}")
+    print(f"Num of Q:{numq}")
+    print(f"Num of relevant Q:{num_relevant_q}")
 
 
 if __name__ == "__main__":
